6052f6e62fb9bd5c2e31d4b47d7f9dceefd9a141
Yuyang Lee <i@aidenli.net>
2022-12-30 15:20:46 +0800

    deduplicate and missing

---
+++ wash.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+import torch
+import torchvision
+import yaml
+
+metadata_path = "dataset/metadata.json"
+img_basedir = "dataset/images"
+
+metadata = json.load(open(metadata_path, "r"))
+json.dump(metadata, open(f"dataset/backup/metadata.{ datetime.now().strftime('%Y-%m-%d_%H:%M:%S') }.json", "w"))
+
+stat = metadata['queries']
+data = metadata['metadata']
+
+actual_stat = { k: 0 for k in stat.keys() }
+paths = { k: [] for k in stat.keys() }
+
+checked = []
+
+error_idxs = []
+for i, v in enumerate(data):
+    hash = f"{v['query']}-{v['source']}-{v['pid']}"
+    if hash in checked:
+        error_idxs.append(i)
+        continue
+    file = os.path.join(v['query'], f"{v['source'].lower()}-{v['pid']}.")
+    ext = "jpg"
+    
+    if v['source'] == 'Pexels':
+        ext = v['extension'] if not v['extension'] == "jpeg" else "jpg"
+        
+    file = file + ext
+    file = file.replace(' ', '-')
+    
+    if os.path.exists(os.path.join(img_basedir, file)):
+        checked.append(hash)
+    else:
+        error_idxs.append(i)
+        
+    paths[v['query']].append(file)
+    
+    actual_stat[v['query']] += 1
+    
+error_idxs = list(set(error_idxs))
+error_idxs.sort()
+
+print(f"Removing {len(error_idxs)} duplicates or missings")
+for rm_i in error_idxs[::-1]:
+    del metadata['metadata'][rm_i]
+    
+print(f"Actual amount of image: { len(metadata['metadata']) }")
+
+metadata['paths'] = paths
+metadata['queries'] = actual_stat
+
+json.dump(metadata, open(metadata_path, "w"))
